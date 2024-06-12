96ad98aa45b0106391919076a98c41464d8808f1
chanhee7 <chan9505@naver.com>
2024-06-12 11:00:55 +0900

    # react props1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-import Expenseltem from "./components/expenses/Expenseltem"
+import Expenseltem from "./components/expenses/Expenseltem";
 
-
-  const App = () => {;
+const App = () => {
   return (
     <>
-      <Expenseltem />
-      <Expenseltem />
+      <Expenseltem title={"치킨"} price={18000} date={new Date(2024, 6, 3)} />
+      <Expenseltem title="햄버거" price={8000} date={new Date(2024, 5, 14)} />
+      <Expenseltem title="피자" price={12000} date={new Date(2024, 4, 27)} />
     </>
   );
-}
+};
 
 export default App;

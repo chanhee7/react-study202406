c89f833af6a669eb8f500509e39e7110ea528138
chanhee7 <chan9505@naver.com>
2024-06-13 12:16:24 +0900

    # 필터된 연도 끌어올리기

--- src/components/expenses/ExpenseList.js
+++ src/components/expenses/ExpenseList.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Expenseltem from "./Expenseltem";
+import ExpenseFilter from "./ExpenseFilter";
 
 const ExpenseList = ({ expenses }) => {
+
+  const onFilterChange = (filteredYear) => {
+
+    // ExpenseFilter에 있는 선택된 연도값을 여기서 출력
+    console.log('ExpenseList: ', filteredYear);
+  }
+
   return (
     <div className="expenses">
+
+      <ExpenseFilter onChangeFilter={onFilterChange} />
+
       <Expenseltem
         title={expenses[0].title}
         price={expenses[0].price}

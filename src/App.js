0a6c07617fcfc56af44666e1ca2aab028c9713c6
chanhee7 <chan9505@naver.com>
2024-06-13 11:53:18 +0900

    # 상향식 데이터 전달

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import ExpenseList from "./components/expenses/ExpenseList";
 import NewExpense from "./components/new-expense/NewExpense";
+import CheckBoxStyle from "./components/practice/CheckBoxStyle";
 
 const App = () => {
   // 서버에서 지출항목 JSON 배열을 응답받음
@@ -23,9 +24,19 @@ const App = () => {
     },
   ];
 
+
+  // ExpenseForm에게 내려보낼 함수
+  const onAddExpense = (userInput) => {
+    console.log('app.js가 내려보낸 함수');
+    // console.log('userInput: ', userInput);
+    expenses.push(userInput);
+    console.log(expenses);
+  }
+
   return (
     <>
-      <NewExpense />
+      <CheckBoxStyle />
+      <NewExpense onSave={onAddExpense} />
       <ExpenseList expenses={expenses} />
     </>
   );

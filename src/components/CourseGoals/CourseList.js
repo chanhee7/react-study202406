2b30778b057c39ffdad96551633c8ce6d744edd1
chanhee7 <chan9505@naver.com>
2024-06-14 11:45:33 +0900

    # 목표 설정 앱 템플릿2

--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './CourseList.css';
+import CourseItem from './CourseItem';
 
-const CourseList = () => {
+const CourseList = ({ items }) => {
   return (
-    <ul className='goal-list'></ul>
+    <ul className='goal-list'>
+      {
+        // map 은 key 넣기
+        items.map(item => <CourseItem key={item.id} item={item} />)
+      }
+    </ul>
   );
 };
 

95130a54e31b57dc474a0573cc0d3ec22bd51645
chanhee7 <chan9505@naver.com>
2024-06-14 10:43:22 +0900

    # 차트 템플릿 완성

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -2,21 +2,20 @@ import React from 'react';
 import ChartBar from './ChartBar';
 import "./Chart.css";
 
-const Chart = () => {
+const Chart = ({ dataPoints }) => {
+
+  /*
+    dataPoints 배열에서 12개 요소의 value를 합산하여 연도 지출총액을 계산
+    그리고 각 ChartBar에 해당월지출총액 / 연도지출총액 비율을 전달
+  */
+
   return (
     <div className='chart'>
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
+      {
+        dataPoints
+          // map 돌릴때 꼭 key 넣기
+          .map(dp => <ChartBar key={dp.label} label={dp.label} />)
+      }
     </div>
   )
 }

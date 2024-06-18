fc87d3462ff8adeaf5b9f450543e98ee805ec454
chanhee7 <chan9505@naver.com>
2024-06-18 09:17:19 +0900

    # Side Effect 템플릿 작성

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React, { useRef ,useState } from "react";
+import React, { useRef, useState } from "react";
+import MainHeader from './components/SideEffect/MainHeader';
+import Home from './components/SideEffect/Home';
+import Login from './components/SideEffect/Login';
 import "./App.css";
-import AddUsers from "./components/Users/AddUsers";
-import UserList from "./components/Users/UserList";
-import ErrorModal from "./components/UI/Modal/ErrorModal";
 
 const App = () => {
-
-  // 회원들이 저장될 배열
-  const [userList, setUserList] = useState([]);
-
-  const addUserHandler = user => {
-
-    console.log(user);
-    setUserList((prev) => [
-      ...prev, 
-      {
-       ...user, 
-       id: Math.random().toString()
-      }
-    ]);
-  };
-
   return (
-    <>
-      <AddUsers onAddUser={addUserHandler} />
-      <UserList users={userList} />
-    </>
+  <>
+    <MainHeader />
+    <main>
+      <Home />
+      {/* {<Login />} */}
+    </main>
+  </>
   );
 };
 

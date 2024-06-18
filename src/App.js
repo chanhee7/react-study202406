8be995fbb95f4888d7b3f517b7495d5bc20f76b9
chanhee7 <chan9505@naver.com>
2024-06-18 09:49:10 +0900

    # 로그인 도중 발생하는 side effect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { useRef, useState } from "react";
-import MainHeader from './components/SideEffect/MainHeader';
-import Home from './components/SideEffect/Home';
-import Login from './components/SideEffect/Login';
+import MainHeader from "./components/SideEffect/MainHeader";
+import Home from "./components/SideEffect/Home";
+import Login from "./components/SideEffect/Login";
 import "./App.css";
 
 const App = () => {
+
+  // 현재 로그인 상태를 체크하는 변수
+  const[isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // localStorage에서 login-flag를 꺼냄
+  const storedLoginFlag = localStorage.getItem('login-flag');
+  // 로그인 검사를 초기에 수행
+  if (storedLoginFlag === '1') {
+    // 상태변수가 setter로 변경되면
+    // 리액트는 변경감지 후 바로 리렌더링을 수행함
+    setIsLoggedIn(true);
+  }
+
+  // 서버 통신은 중앙집중 관리가 중요함
+  const loginHandler = (email, password) => {
+    // 로그인의 증거로 클라이언트에 1이라는 숫자를 기록
+    localStorage.setItem('login-flag', '1');
+    setIsLoggedIn(true);
+  };
+
   return (
-  <>
-    <MainHeader />
-    <main>
-      {/* <Home /> */}
-      {<Login />}
-    </main>
-  </>
+    <>
+      <MainHeader />
+      <main>
+        {isLoggedIn && <Home />}
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+      </main>
+    </>
   );
 };
 

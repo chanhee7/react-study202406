f3de5fe7fca0d1d6e7b2c0bd7c137247f602d5f3
chanhee7 <chan9505@naver.com>
2024-06-24 10:24:28 +0900

    # 헤더 총 수량 배지 렌더링

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import CartIcon from './CartIcon'
-import styles from './HeaderCareButton.module.scss'
+import React, { useContext } from "react";
+import CartIcon from "./CartIcon";
+import styles from "./HeaderCareButton.module.scss";
+import CartContext from "../../../store/card-context";
 
 const HeaderCartButton = ({ onShow }) => {
 
-  const {button, icon, badge} = styles;
+  const { cartItems } = useContext(CartContext);
+
+  // 나의 장바구니 수량 렌더링 1번째 방법
+  const calcTotalAmount = () => {
+    let totalAmount = 0;
+    for (const item of cartItems) {
+      totalAmount += item.amount;
+    }
+    return totalAmount;
+  };
+
+  // 나의 장바구니 수량 렌더링 2번째 방법
+  // const numberOfCart = cartItems.reduce((accum, current) => {
+  //   console.log('accum: ', accum);
+  //   console.log('current: ', current);
+  //   return accum + current.amount;
+  // }, 0);
+
+
+  const { button, icon, badge } = styles;
 
   return (
     <button className={button} onClick={onShow}>
@@ -12,9 +32,12 @@ const HeaderCartButton = ({ onShow }) => {
         <CartIcon />
       </span>
       <span>My Cart</span>
-      <span className={badge}>3</span>
+      {/* // 나의 장바구니 수량 렌더링 1번째 방법 */}
+      <span className={badge}>{calcTotalAmount()}</span>
+      {/* // 나의 장바구니 수량 렌더링 2번째 방법 */}
+      {/* <span className={badge}>{numberOfCart}</span> */}
     </button>
-  )
-}
+  );
+};
 
-export default HeaderCartButton
+export default HeaderCartButton;

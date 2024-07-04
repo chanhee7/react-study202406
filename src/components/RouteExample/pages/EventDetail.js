9bed579d19bf8bc0969cfeda3df8d976202564be
chanhee7 <chan9505@naver.com>
2024-07-04 10:23:09 +0900

    # loader로 상세조회하기

--- src/components/RouteExample/pages/EventDetail.js
+++ src/components/RouteExample/pages/EventDetail.js
@@ -4,19 +4,28 @@ import EventItem from "../components/EventItem";
 
 const EventDetail = () => {
 
-  // 주소에 전달된 파라미터 읽기
-  const { eventId: id } = useParams();
-  const [ev, setEv] = useState({});
+  const ev = useLoaderData();
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`http://localhost:8282/events/${id}`);
-      const json = await response.json();
-      setEv(json);
-    })();
-  }, []);
-
-  return <EventItem event={ev}/>
+  return <EventItem event={ev} />;
 };
 
 export default EventDetail;
+
+export const loader = async ({ params }) => {
+
+  const { eventId: id } = params;
+
+  // console.log('abc: ', abc.params.eventId);
+
+  // use로 시작하는 함수인 리액트 훅은 컴포넌트 내부에서만 사용가능
+  // const { eventId: id } = useParams();
+  // const [ev, setEv] = useState({});
+
+  const response = await fetch(`http://localhost:8282/events/${id}`);
+
+  if (!response.ok) {
+    // ... 예외처리
+  }
+
+  return await response.json();
+};
